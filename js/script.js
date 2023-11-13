19055a8ea0fe01a183598fe04fa32bebaaf626a9
RicTieri <tieri.riccardo@gmail.com>
2023-11-13 15:48:35 +0100

    added script based on previous ex

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
+const buttonElement = document.querySelector('button')
 
-let tripCost = roadMapLength * 0.21;
-let tripPrice;
-let discount;
+buttonElement.addEventListener('click', function(){
+  const roadMapLength = parseFloat(document.getElementById('user_distance').value)
+  const guestAge = parseFloat(document.getElementById('user_age').value)
+  let tripCost = roadMapLength * 0.1976;
+  let tripPrice;
+  let discount;
+  
+  if (guestAge < 18) {
+    tripPrice = tripCost * 0.8235
+    discount = '17,65%'
+  } else if (guestAge > 65) {
+    tripPrice = tripCost * 0.4673
+    discount = '53,27%'
+  } else {
+    tripPrice = tripCost
+    discount = '0%'
+  }
+  console.log(tripPrice.toFixed(2)); 
+})
 
-if (guestAge < 18) {
-  tripPrice = tripCost * 0.8
-  discount = '20%'
-} else if (guestAge > 65) {
-  tripPrice = tripCost * 0.6
-  discount = '40%'
-} else {
-  tripPrice = tripCost
-  discount = '0%'
-}
 
-document.getElementById('guest_age').innerHTML = 'Età passeggiero: ' + guestAge;
-document.getElementById('trip_length').innerHTML = 'La distanza da percorrere è ' + roadMapLength + ' km';
-document.getElementById('trip_cost').innerHTML = 'Il totale lordo è ' + tripCost.toFixed(2) + '\u20AC';
-document.getElementById('trip_discount').innerHTML = 'Lo sconto applicato è ' + discount;
-document.getElementById('trip_price').innerHTML = 'Il totale effettivo è ' + tripPrice.toFixed(2) + '\u20AC';
