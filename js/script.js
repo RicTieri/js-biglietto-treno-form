73e71dfb75e00fa5cb13e3b46ceb2460884c47ff
RicTieri <tieri.riccardo@gmail.com>
2023-11-13 16:55:13 +0100

    added output

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
 const buttonElement = document.querySelector('button')
 
-buttonElement.addEventListener('click', function(){
+buttonElement.addEventListener('click', function () {
   const userName = document.getElementById('user_name').value;
   const roadMapLength = parseFloat(document.getElementById('user_distance').value);
   const guestAge = parseFloat(document.getElementById('user_age').value);
   let tripCost = roadMapLength * 0.1976;
   let tripPrice;
   let discount;
-  
+
   if (guestAge < 18) {
     tripPrice = tripCost * 0.8235
-    discount = '17,65%'
-  } else if (guestAge > 65) {
+    discount = 'Sconto under18'
+  } else if (guestAge > 64) {
     tripPrice = tripCost * 0.4673
-    discount = '53,27%'
+    discount = 'Sconto over65'
   } else {
     tripPrice = tripCost
-    discount = '0%'
+    discount = 'Tariffa base'
   }
-  console.log(tripPrice.toFixed(2)); 
+  console.log(tripPrice.toFixed(2));
   console.log(userName)
+
+  document.getElementById('passenger').innerHTML = userName;
+  document.getElementById('offer').innerHTML = discount;
+  document.getElementById('cp_code').innerHTML = Math.floor(Math.random() * 99999)+1;
+  document.getElementById('carriage').innerHTML = Math.floor(Math.random() * 10);
+  document.getElementById('lastprice').innerHTML = tripPrice.toFixed(2);
 })
 
 
